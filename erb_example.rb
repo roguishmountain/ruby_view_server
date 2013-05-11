462fd5745e9815825e19f73c02fa2f1f40356edd
Sam <roguishmountain@gmail.com>
2013-05-10 22:35:49 -0400

    added more examples of erb

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,14 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+arr = ['cats', 'dogs', 'birds']
+
+arr.each do |x|
+	str = ERB.new "arr is <%= x %>"
+	puts str.result(binding)
+end
+
+s = "hello"
+str = ERB.new "<h2> <%= s %>  </h2>"
+puts str.result(binding)
