2201d5e89509d23c6f01543ccd4309fae4e8895d
Sam <roguishmountain@gmail.com>
2013-08-31 11:35:35 -0400

    added hashes to example

--- erb_example.rb
+++ erb_example.rb
@@ -5,6 +5,12 @@ template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
 
 arr = ['cats', 'dogs', 'birds']
+hash = {"one" => 1, "two" => 2}
+
+hash.each do |x,y|
+	str = ERB.new "hash is <%= x %> => <%= y %>"
+	puts str.result(binding)
+end
 
 arr.each do |x|
 	str = ERB.new "arr is <%= x %>"
